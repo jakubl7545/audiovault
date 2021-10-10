964b36b12ed6660f78f08e96077198760f112f79
jakubl7545 <jakubl7545@gmail.com>
2021-10-10 15:02:13 +0200

    Improve description generator

--- app/description_generator.py
+++ app/description_generator.py
@@ -1,5 +1,18 @@
+import re
 from imdb import IMDb
+
 ia = IMDb()
+
+def prepare_title(title):
+	pattern1 = '(- Season .*)'
+	pattern2 = '( \[.*\] )'
+	if re.search(pattern1, title):
+		parts = re.split(pattern1, title)
+		title =''.join(parts[0])
+	elif re.search(pattern2, title):
+		parts = re.split(pattern2, title)
+		title = ' '.join((parts[0], parts[2]))
+	return title
 
 def get_id(title):
 	movies = ia.search_movie(title)
@@ -7,7 +20,7 @@ def get_id(title):
 
 def get_description(title):
 	try:
-		id = get_id(title)
+		id = get_id(prepare_title(title))
 		description = ia.get_movie(id)['plot'][0]
 	except:
 		return "Unable to generate description"
