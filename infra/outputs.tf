dda63b702637fc0a9d6df2f8ec2d77663e3c9b48
jakubl7545 <jakubl7545@gmail.com>
2025-05-30 14:04:18 +0200

    Add CSI driver for EFS storage to the cluster

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "db_endpoint" {
   description = "rds db endpoint"
   value = aws_db_instance.audiovault_rds.endpoint
 }
+
+output "efs_id" {
+  description = "EFS file system id"
+value = aws_efs_file_system.audiovault_efs.id
+}
