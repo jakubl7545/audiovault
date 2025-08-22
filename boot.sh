7bcf5f1a77a81acb54c26962bd28a0a77582a888
jakubl7545 <jakubl7545@gmail.com>
2025-08-22 18:52:35 +0200

    Use asynchronous workers in gunicorn to handle multiple concurrent requests

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 flask db upgrade
-exec gunicorn -b :5000 -t 0 --access-logfile - --error-logfile - app:app
+exec gunicorn -b :5000 -k gevent --access-logfile - --error-logfile - app:app
